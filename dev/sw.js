da5fa9a01b43588854c5fb8173adefa19e7dfdda
Jayydoodle <Jayydoodle@gmail.com>
2020-04-17 18:47:40 -0400

    Updates

---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-game-project-team-12-jason-and-shabir/devprecache-manifest.83c26fcb44d34ea5bab93f90521cc9f0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
